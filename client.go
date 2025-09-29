4b5c3ecb40b09e72790a383c87211ce7fc6b06b4
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2025-09-29 16:50:06 -0600

    init

---
+++ client.go
@@ -0,0 +1,21 @@
+package client
+
+import "net/http"
+
+type LandscapeAPIClient struct {
+	accountName string
+	rootURL     string
+	accessKey   string
+	secretKey   string
+	http        *http.Client
+}
+
+func NewClient(accountName, rootURL, accessKey, secretKey string) *LandscapeAPIClient {
+	return &LandscapeAPIClient{
+		accountName: accountName,
+		rootURL:     rootURL,
+		accessKey:   accessKey,
+		secretKey:   secretKey,
+		http:        &http.Client{},
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jansdhillon/landscape-go-client
+
+go 1.24.6
